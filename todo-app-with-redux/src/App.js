35d9b28be77d120cc8da4cdf448c2689246fc35a
Raymond <96218923+raymond-le-goldenowl@users.noreply.github.com>
2022-01-05 12:43:27 +0700

    React with redux (#6)
    * Add react app
    
    * React with redux

---
+++ todo-app-with-redux/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import TodoApp from "./containers/TodoApp";
+
+function App() {
+  return (
+    <div className="app">
+       <h1>Hello</h1>
+      <TodoApp />
+    </div>
+  );
+}
+
+export default App;

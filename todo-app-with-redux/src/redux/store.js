c8211aed310e8f1e55a70c1ea2e1ad3bf87b7f62
raymond-le-goldenowl <raymond.le.goldenowl@gmail.com>
2022-01-07 17:20:42 +0700

    fix: change from asyncMiddleware to thunk

--- todo-app-with-redux/src/redux/store.js
+++ todo-app-with-redux/src/redux/store.js
@@ -1,18 +1,13 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
 import todoReducer from "./todo";
 import { composeWithDevTools } from "redux-devtools-extension";
 const reducer = combineReducers({ todo: todoReducer });
-const composedEnhancers = composeWithDevTools();
 const myMiddleware = (store) => (next) => (action) => {
   return next(action);
 };
-const asyncMiddleware = (store) => (next) => (action) => {
-  if (typeof action === "function") {
-    return action(next);
-  }
-  return next(action);
-};
+
 export default createStore(
   reducer,
-  applyMiddleware(myMiddleware, asyncMiddleware)
+  applyMiddleware(myMiddleware, thunk)
 );

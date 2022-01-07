fa869cc29b1945785f5b33a6ea28f86167434c81
Raymond <96218923+raymond-le-goldenowl@users.noreply.github.com>
2022-01-07 16:07:05 +0700

    feat: add redux middleware (#10)

--- todo-app-with-redux/src/redux/store.js
+++ todo-app-with-redux/src/redux/store.js
@@ -1,6 +1,11 @@
-import { createStore, combineReducers } from "redux";
+import { createStore, combineReducers, applyMiddleware } from "redux";
 import todoReducer from "./todo";
 import { composeWithDevTools } from "redux-devtools-extension";
 const reducer = combineReducers({ todo: todoReducer });
 const composedEnhancers = composeWithDevTools();
-export default createStore(reducer, composedEnhancers);
+const myMiddleware = (store) => (next) => (action) => {
+  console.log("action", action);
+  console.log("store", store);
+  return next(action);
+};
+export default createStore(reducer, applyMiddleware(myMiddleware));

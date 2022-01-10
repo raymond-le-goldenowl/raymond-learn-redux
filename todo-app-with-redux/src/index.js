d18da2631c3d99adf179599cb87572cbaa5555eb
raymond-le-goldenowl <raymond.le.goldenowl@gmail.com>
2022-01-10 16:02:46 +0700

    feat: add redux persist

--- todo-app-with-redux/src/index.js
+++ todo-app-with-redux/src/index.js
@@ -3,10 +3,14 @@ import ReactDOM from "react-dom";
 import App from "./containers/App";
 import "./index.css";
 import { Provider } from "react-redux";
-import store from './redux/store'
+import store, { persistor } from "./redux/store";
+import { PersistGate } from "redux-persist/integration/react";
+
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <PersistGate load={null} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>,
   document.getElementById("root")
 );
--- todo-app-with-redux/src/redux/store.js
+++ todo-app-with-redux/src/redux/store.js
@@ -1,10 +1,20 @@
 import { combineReducers } from "redux";
 import { configureStore } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
 import todoReducer from "./todo";
 import authReducer from "./auth";
 const reducer = combineReducers({ todo: todoReducer, auth: authReducer });
 
+const persistConfig = { key: "root", storage, whitelist: ["auth"] };
+const persistedReducer = persistReducer(persistConfig, reducer);
+
 //* auto setup thunk, redux devtools, ...
-export default configureStore({
-  reducer,
+const store = configureStore({
+  reducer: persistedReducer,
 });
+
+export const persistor = persistStore(store);
+
+export default store;

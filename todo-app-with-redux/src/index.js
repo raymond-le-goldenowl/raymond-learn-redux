2ee4ca49c6595b10dba267be64970f7ea9561ca0
raymond-le-goldenowl <raymond.le.goldenowl@gmail.com>
2022-01-14 11:42:37 +0700

    setup

--- todo-app-with-redux/src/index.js
+++ todo-app-with-redux/src/index.js
@@ -1,16 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./containers/App";
+import App from "./App";
 import "./index.css";
-import { Provider } from "react-redux";
-import store, { persistor } from "./redux/store";
-import { PersistGate } from "redux-persist/integration/react";
 
-ReactDOM.render(
-  <Provider store={store}>
-    <PersistGate load={null} persistor={persistor}>
-      <App />
-    </PersistGate>
-  </Provider>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
